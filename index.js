f7e252678c3d4c27d9dc10ac3b0a543b834fb039
fasmirhi <mohamedfasmir2024@gmail.com>
2024-05-30 01:30:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const users = require("./sample.json");
+const cors = require("cors");
+
+
+const app = express();
+const port = 8000;
+app.use(
+    cors ({
+        origin: "http://localhost:5173/",
+        methods: ["GET", "POST", "PATCH"],
+
+    })
+
+);
+
+app.get("/users",(req,res) =>{
+    return res.json(users);
+});
+
+app.listen(port,(err) =>{
+    console.log(`App is running in port ${port}`);
+}
+);
